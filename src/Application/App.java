d6ff48f55fb78a539f788f6543d44586aa924ec2
RibeiroFilho92 <ribeiro.pdf@hotmail.com>
2020-10-30 19:41:20 -0300

    Method delete, type seller, implemented

--- src/Application/App.java
+++ src/Application/App.java
@@ -2,16 +2,18 @@ package Application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import Model.Dao.DAOFactory;
 import Model.Dao.SellerDao;
-import Model.Dao.implementation.SellerDaoJDBC;
 import Model.Entities.Department;
 import Model.Entities.Seller;
 
 public class App {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DAOFactory.createSellerDao();
 		System.out.println("Test one: find Seller by ID: ");
@@ -36,11 +38,20 @@ public class App {
 		sellerDao.insert(istdSeller);
 		System.out.println("New seller inserted: ID generated " + istdSeller.getID());
 		
-		System.out.println("Test fiver: alter seller data: ");
+		System.out.println("Test five: alter seller data - UPDATE: ");
 		seller = sellerDao.findByID(1);
 		seller.setName("Marta");
 		sellerDao.update(seller);
 		System.out.println("Updated");
+		
+		System.out.println("Test six: alter seller data - DELETE: ");
+		System.out.println("Insert the Seller ID to be deleted: ");
+		int ID = sc.nextInt();
+		sellerDao.deleteByID(ID);
+		System.out.println("Deleted");
+		
+		sc.close();
+		
 	}
 
 }
